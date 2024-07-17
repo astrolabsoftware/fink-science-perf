5c5aec3e6fe0c79fedbbeb1d5edfd89753d0aa72
JulienPeloton <peloton@lal.in2p3.fr>
2024-07-17 09:55:03 +0200

    Add exec

---
+++ run_benchmark.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+FILENAME=""
+DATAFOLDER=""
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -f)
+        FILENAME="$2"
+        shift 2
+        ;;
+    -d)
+	DATAFOLDER="$2"
+	shift 2
+	;;
+  esac
+done
+
+if [[ $FILENAME == "" ]]; then
+  echo "You need to specify a filename with the argument -f"
+  exit
+fi
+
+if [[ $DATAFOLDER == "" ]]; then
+  echo "You need to specify a data folder with the argument -d"
+  exit
+fi
+
+
+kernprof -l $FILENAME -d $DATAFOLDER
+python -m line_profiler $FILENAME.lprof
